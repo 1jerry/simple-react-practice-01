e544c571e08e78258631b69f5c8e44161b2de6ae
Jerry McRae <git-73ry@sneakemail.com>
2021-09-02 04:41:25 -0700

    ask for list

--- src/App.js
+++ src/App.js
@@ -2,15 +2,33 @@ import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [value,update] = useState('unknown')
-  const change = e => {update(e.target.value)}
+  const [value, update] = useState();
+  const [isSolved, setSolved] = useState(false);
+  const [solution, setSolution] = useState("");
+  const [notice, setNotice] = useState("");
+  const change = (e) => {
+    const answer = e.target.value;
+    let result = "Invalid input.";
+    if (answer.replaceAll(" ", "").match(/^([0-9]+,?)+[0-9]$/)) {
+      result = "Valid input.";
+    }
+    update(answer);
+    setNotice(result);
+  };
   return (
     <div className="App">
-      <h1>Hello {value}</h1>
-      <label > What is your name?  
-      <input type="text" value={value} onChange={change} />
+      <h1>Hello. {notice}</h1>
+      <label>
+        {" "}
+        Enter a list of at least three numbers:
+        <input
+          type="text"
+          value={value}
+          onChange={change}
+          placeholder="numbers and commas only"
+        />
       </label>
-      <h2>Start editing to see some magic happen!</h2>
+      {isSolved ? <h2>here</h2> : <h2>not solved</h2>}
     </div>
   );
 }
